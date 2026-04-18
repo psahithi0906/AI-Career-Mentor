61a0e041931ceeebcbcddf74a4ac841effc78363
Soumya Reddy <sunsowmya24@gmail.com>
2026-04-18 15:14:55 +0530

    Added Home Page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,17 @@
+import Home from "./Home";
+import hero from "../public/hero.png";
+
 export default function App() {
+  const sectionStyle = {
+    width: "100%",
+    height: "700px",
+    backgroundImage: `url(${hero})`,
+    backgroundSize : 'cover',
+    backgroundPosition: 'center'
+  };
   return (
-    <div>
-      <h1>Hello World 🚀</h1>
+    <div style = {sectionStyle}>
+      <Home />
     </div>
   );
 }
---
+++ frontend/src/Home.jsx
@@ -0,0 +1,47 @@
+import "./Home.css";
+
+function Home() {
+  return (
+    <div className="home">
+      {/* Navbar */}
+      <nav className="navbar">
+        <div className="logo">AI Career</div>
+        <ul className="nav-links">
+          <li>Features</li>
+          <li>Pricing</li>
+          <li>Contact</li>
+          <button className="login-btn">Sign In</button>
+        </ul>
+      </nav>
+
+      {/* Hero Section */}
+      <div className="hero">
+        <h1>
+          Empower Your Career with <span>AI</span>
+        </h1>
+        <p>
+          Get personalized career paths and resume analysis powered by AI.
+        </p>
+
+        <div className="buttons">
+          <button className="primary-btn">Get Started</button>
+          <button className="secondary-btn">Watch Demo</button>
+        </div>
+
+        {/* Image
+        <div className="hero-image">
+          <img src="/hero.png" alt="career" />
+        </div> */}
+      </div>
+
+      {/* Features */}
+      <div className="features">
+        <button className="options">🎯 Personalized Career Paths</button>
+        <button className="options">📄 AI Resume Analysis</button>
+        <button className="options">💡 Skill Recommendations</button>
+      </div>
+    </div>
+  );
+}
+
+export default Home;
